625d4d8bf5b798f771f6b56c01063baa8857d3c3
viniciuslevi <71188511+viniciuslevi@users.noreply.github.com>
2024-06-14 12:12:28 -0300

    Update colorsSorter.ino

--- colorsSorter.ino
+++ colorsSorter.ino
@@ -2,38 +2,76 @@
 
 #define pinoServo1 5 // Pinos dos servo-Motores
 #define pinoServo2 6
-#define s0 8       // Pino do módulo s0
-#define s1 9       // Pino do módulo s1
-#define s2 10      // Pino do módulo s2
-#define s3 11      // Pino do módulo s3
+#define S0 8       // Pino do módulo s0
+#define S1 9       // Pino do módulo s1
+#define S2 10      // Pino do módulo s2
+#define S3 11      // Pino do módulo s3
 #define out 12     // Pino do módulo out 
-
+#define speed 1
 #define LONG 1000 // Definindo intervalos de tempo
 #define SHORT 100
 
+struct Pair {
+  int first;
+  int second;
+}; //Criando a estutura de pares
+Pair matrix[4][3]; //Declarando a matriz de espectro de cores
+
+  
 int data = 0;      // Variável para armazenar os valores lidos
 Servo servo1, servo2; // Declaração do tipo servo
 
-int servo2Position = 0;
-int servo1Position = 0;
+const int initialServo2Position = 0; //Angulo inicial do servomotor 1
+const int initialServo1Position = 108; //Angulo inicial do servomotor 2
+const int sensorPosition = 57; //angulo onde se escontra o sensor de cores
+  
+int servo2Position; //Declaração de indices das posições dos servos
+int servo1Position;
 
-void setup() {
+int counter=0; //Declaracao de variaveis utilizadas na ETAPA 2
+int color;
+int frequency;
+
+void setup() {  
+  
+   //configuração cor vermelho
+  matrix[0][0].first = 36; matrix[0][0].second = 46; //minimo e maximo do Red
+  matrix[0][1].first = 39; matrix[0][1].second = 50;
+  matrix[0][2].first = 25; matrix[0][2].second = 34;
+
+   //configuração cor verde
+  matrix[1][0].first = 32; matrix[1][0].second = 38; //configuração cor verde
+  matrix[1][1].first = 24; matrix[1][1].second = 31;
+  matrix[1][2].first = 18; matrix[1][2].second = 21;
+
+   //configuração cor azul
+  matrix[2][0].first = 47; matrix[2][0].second = 53; //configuração cor azul
+  matrix[2][1].first = 37; matrix[2][1].second = 45;
+  matrix[2][2].first = 25; matrix[2][2].second = 30;
+
+   //configuração cor branco
+  matrix[3][0].first = 21; matrix[3][0].second = 31; // configuração cor branca
+  matrix[3][1].first = 18; matrix[3][1].second = 28;
+  matrix[3][2].first = 11; matrix[3][2].second = 21;
+
   servo1.attach(pinoServo1); // Associação do pino digital ao objeto do tipo servo
-  servo1.write(0); // Inicia o motor na posição 0º
-  
+  servo1.write(initialServo1Position); // Inicia o motor na posição 0º
+  delay(speed*250); 
+
   servo2.attach(pinoServo2); // Associação do pino digital ao objeto do tipo servo
-  servo2.write(0); // Inicia o motor na posição 0º
+  servo2.write(initialServo2Position); // Inicia o motor na posição 0º
   
-  pinMode(s0, OUTPUT); // Define o modo dos pinos s0, s1, s2, s3 como saída
-  pinMode(s1, OUTPUT);
-  pinMode(s2, OUTPUT);
-  pinMode(s3, OUTPUT);
+  pinMode(S0, OUTPUT); // Define o modo dos pinos s0, s1, s2, s3 como saída
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
   pinMode(out, INPUT); // Define o modo do pino out como entrada
+  //pinMode(4, INPUT);
 
   Serial.begin(9600); // Inicializa a comunicação serial com baud rate de 9600
   
-  digitalWrite(s0, HIGH); // Configura a escala de frequência de saída do sensor para 100%
-  digitalWrite(s1, HIGH);
+  digitalWrite(S0, HIGH); // Configura a escala de frequência de saída do sensor para 100%
+  digitalWrite(S1, HIGH);
 }
 
 void loop() {
@@ -44,76 +82,119 @@ void loop() {
 }
 
 void ETAPA4() {
-  for (int servo1Position = 60; servo1Position < 90; servo1Position++) { // Para "pos" igual a 60, enquanto "pos" menor que 180, incrementa "pos"
+  for (int servo1Position = sensorPosition; servo1Position >= 0; servo1Position--) { // Para "pos" igual a posicao do sensor, enquanto "pos" maior que 0, decrementa "pos"
     servo1.write(servo1Position); // Escreve o valor da posição que o servo deve girar
-    delay(15); // Intervalo de 15 milissegundos
+    delay(speed*10); // Intervalo de 15 milissegundos
   }
-  delay(1000); // Intervalo de 1 segundo
+  delay(speed*LONG); // Intervalo de 1 segundo, para que dê tempo da peça cair na rampa
   
-  for (int servo1Position = 90; servo1Position >= 0; servo1Position--) { // Vai de 180 graus para 0 graus
+  for (int servo1Position = 0; servo1Position <= initialServo1Position; servo1Position++) { // Vai de 0 graus para 108 graus, retornando para a posição inicial
     servo1.write(servo1Position); // Diz ao servo para ir para a posição na variável 'pos'
-    delay(15); // Espera 15ms para o servo alcançar a posição
+    delay(speed*10); // Espera 15ms para o servo alcançar a posição
   }
-  delay(SHORT);
+  delay(speed*SHORT);
 }
 
 void ETAPA3(int color) { // Função responsável por rotacionar a rampa
   while (servo2Position > 30 * color) {
     servo2.write(servo2Position);
-    delay(15);
+    delay(speed*10);
     servo2Position--;
   }
   while (servo2Position < 30 * color) {
     servo2.write(servo2Position);
-    delay(15);
+    delay(speed*10);
     servo2Position++;
   }
-  delay(SHORT);
+  delay(speed*SHORT);
 }
 
-int ETAPA2() {    
-  int Red, Blue, Green;
-  int color = 0; // 1:vermelho; 2:azul, 3:verde
-  
-  // Leitura dos valores RGB
-  digitalWrite(s2, LOW); // Seleciona os fotodiodos vermelhos
-  digitalWrite(s3, LOW);
-  Red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH); // Mede a duração do pulso LOW
-  delay(20);  
-   
-  digitalWrite(s3, HIGH); // Seleciona os fotodiodos azuis
-  Blue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
-  delay(20);  
-   
-  digitalWrite(s2, HIGH); // Seleciona os fotodiodos verdes
-  Green = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
-  delay(20);  
 
-  // Identificação da cor com base nos valores RGB lidos
-  if (Red <= 15 && Green <= 15 && Blue <= 15) {
-    Serial.println("White"); // Se todos os valores forem baixos, é provável que seja branco
-    color = 0;
-  } else if (Red < Blue && Red <= Green && Red < 23) {
-    Serial.println("Red"); // Se o valor de vermelho for o mais baixo e menor que 23, é vermelho
-    color = 1;
-  } else if (Blue < Green && Blue < Red && Blue < 20) {
-    Serial.println("Blue"); // Se o valor de azul for o mais baixo e menor que 20, é azul
-    color = 2;
-  } else if (Green < Red && (Green - Blue) <= 8) {
-    Serial.println("Green"); // Se o verde for menor que o vermelho e a diferença entre verde e azul for aceitável, é verde
-    color = 3;
-  } else {
-    Serial.println("Unknown"); // Se não for reconhecida, imprime "Unknown"
-    ETAPA2(); // Enquanto não identificar a cor, continua a leitura até encontrar
-  }
-   
-  return color;
+
+int ETAPA2() {    
+  // Configurando fotodiodos filtrados para vermelho
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, LOW);
+  // Lendo a frequência de saída
+  int red = frequency = pulseIn(out, LOW);
+  // Imprimindo o valor no monitor serial
+  Serial.print("R= ");
+  Serial.print(frequency); // imprimindo frequência da cor vermelha
+  Serial.print("  ");
+  delay(speed*100);
+
+  // Configurando fotodiodos filtrados para verde
+  digitalWrite(S2, HIGH);
+  digitalWrite(S3, HIGH);
+  // Lendo a frequência de saída
+  int green = frequency = pulseIn(out, LOW);
+  // Imprimindo o valor no monitor serial
+  Serial.print("G= ");
+  Serial.print(frequency); // imprimindo frequência da cor verde
+  Serial.print("  ");
+  delay(speed*100);
+
+  // Configurando fotodiodos filtrados para azul
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, HIGH);
+  // Lendo a frequência de saída
+  int blue = frequency = pulseIn(out, LOW);
+  // Imprimindo o valor no monitor serial
+  Serial.print("B= ");
+  Serial.print(frequency); // imprimindo frequência da cor azul
+  Serial.println("  ");
+  delay(speed*100);
+
+  // Verificando se a cor é vermelha
+  if(red >= matrix[0][0].first && red <= matrix[0][0].second
+    && green >= matrix[0][1].first && green <= matrix[0][1].second
+    && blue >= matrix[0][2].first && blue <= matrix[0][2].second) {
+      Serial.println("Red"); // Identificado como vermelho
+      color = 1;
+    }
+  // Verificando se a cor é verde
+  else if(red >= matrix[1][0].first && red <= matrix[1][0].second
+    && green >= matrix[1][1].first && green <= matrix[1][1].second
+    && blue >= matrix[1][2].first && blue <= matrix[1][2].second) {
+      Serial.println("Green"); // Identificado como verde
+      color = 3;
+    }
+  // Verificando se a cor é azul
+  else if(red >= matrix[2][0].first && red <= matrix[2][0].second
+    && green >= matrix[2][1].first && green <= matrix[2][1].second
+    && blue >= matrix[2][2].first && blue <= matrix[2][2].second) {
+      Serial.println("Blue"); // Identificado como azul
+      color = 2;
+    }
+  // Verificando se a cor é branca
+  else if(red >= matrix[3][0].first && red <= matrix[3][0].second
+    && green >= matrix[3][1].first && green <= matrix[3][1].second
+    && blue >= matrix[3][2].first && blue <= matrix[3][2].second) 
+    {
+      Serial.println("White"); // Identificado como branco
+      color = 0; 
+    }
+  // Se a cor for desconhecida
+  else {
+      Serial.println("Desconhecida");
+      if((counter++) >= 50) {
+        counter = 0;
+        // Movendo servo1 para posição inicial se não for identificada cor após 50 tentativas
+        for (int servo1Position = sensorPosition; servo1Position <= initialServo1Position; servo1Position++) {
+          servo1.write(servo1Position); // Movendo servo1
+          delay(speed*10); // Espera para alcançar a posição
+        }
+        while(true); // Parando o programa
+      }
+      ETAPA2(); // Repetindo a leitura até encontrar a cor
+    }
+    return color;
 }
 
 void ETAPA1() {
-  for (int servo1Position = 0; servo1Position < 60; servo1Position++) { // Para "pos" igual a 0, enquanto "pos" menor que 60, incrementa "pos"
+  for (int servo1Position = initialServo1Position; servo1Position >= sensorPosition; servo1Position--) { // Para "pos" igual a posição inicial, enquanto "pos" menor que a posicao do sensor, decrementa "pos"
     servo1.write(servo1Position); // Escreve o valor da posição que o servo deve girar
-    delay(15); // Intervalo de 15 milissegundos
+    delay(speed*10); // Intervalo de 15 milissegundos
   }
-  delay(SHORT); // Intervalo de 100ms
+  delay(speed*SHORT); // Intervalo de 100ms
 }
